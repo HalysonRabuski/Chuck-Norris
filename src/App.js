5688d9906c2a0f54af20df1002f5d0d9c64258b2
halysonrabuski <rabuskihalyson@gmail.com>
2020-02-29 00:34:05 -0300

    loading spinner

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import  api from    './services/api';
+import HashLoader from "react-spinners/HashLoader";
+
 
 import chuck from "./assets/chuck.png"
 export default function App() {
   const   [sentence, setSentence]   =   useState("");
+  const   [loading, setLoading]   =   useState(false);
+
+  useEffect(()=>{
+    loadSentence()
+  },[])
 
   async function loadSentence(){
+    setLoading(true)
     const response  = await api.get("https://api.chucknorris.io/jokes/random")
+    setLoading(false)
     setSentence(response.data.value)
   }
   
@@ -19,6 +28,13 @@ export default function App() {
     <div className="button">
       <button onClick={loadSentence}>Click to generate a chuck norris joke</button>
       <p>{sentence}</p>
+      <HashLoader
+          css={{marginLeft: 'auto', marginRight: 'auto'}}
+          size={125}
+          //size={"150px"} this also works
+          color={"#123abc"}
+          loading={loading}
+        />
     </div>
     </div>
    
